3c0d1fe1fdf3054083e4fd08fb02fec7aff0ecc7
Sergey Koshkin <koshkin.sergey@gmail.com>
2017-10-13 23:48:11 +0300

    Updated the license

--- Device/ADI/ADuCM32x/CMSIS_Driver/UART_ADuCM320.c
+++ Device/ADI/ADuCM32x/CMSIS_Driver/UART_ADuCM320.c
@@ -1,9 +1,20 @@
 /*
- * Copyright (C) 2017 Sergey Koshkin <sergey.koshkin@neophotonics.com>
+ * Copyright (C) 2017 Sergey Koshkin <koshkin.sergey@gmail.com>
  * All rights reserved
  *
- * File Name  : UART_ADuCM320.c
- * Description: UART Driver for ADI ADuCM320
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Project: UART Driver for ADI ADuCM32x
  */
 
 /*******************************************************************************
